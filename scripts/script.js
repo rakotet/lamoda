a647e9664b7765c65fd72dcc24abcc6dbed41033
rakotet <64151502+rakotet@users.noreply.github.com>
2021-07-09 10:43:31 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,6 @@ const headerCityButton = document.querySelector('.header__city-button')
 
 let hash = location.hash.substring(1) // определяем хешь страницы (значение после # в конце ссылки на страницу) и сразу обрезаем первый его элемент (#)
 
-if(hash === 'women') document.querySelector('.goods__title').textContent = 'Женщинам'
-else if(hash === 'men') document.querySelector('.goods__title').textContent = 'Мужчинам'
-else if(hash === 'kids') document.querySelector('.goods__title').textContent = 'Детям'
-
 if(localStorage.getItem('lamoda-location')) {
     headerCityButton.textContent = localStorage.getItem('lamoda-location')
 }
@@ -22,8 +18,14 @@ headerCityButton.addEventListener('click', () => {
 
 const disableScroll = () => {
     // Делаем все эти манипуляции для устранения багов и рывков при скрытии скрола (эти ф-и можно использовать в других проэктах)
+    if(document.disableScroll) return
+
     const widthScroll = window.innerWidth - document.body.offsetWidth // находим ширину скрола (ширина окна браузера с скролом - ширина страницы без скрола)
+
+    document.disableScroll = true
+
     document.body.dbScrollY = window.scrollY // создаём и записываем в новое поле dbScrollY объекта body количество пикселей которое мы отматали сверху страницы скролом
+
     document.body.style.cssText = `
         position: fixed;
         top: ${-window.scrollY}px;
@@ -36,6 +38,8 @@ const disableScroll = () => {
 }
 
 const enableScroll = () => {
+    document.disableScroll = false
+
     document.body.style.cssText = ''
     window.scroll({
         top: document.body.dbScrollY
@@ -50,6 +54,13 @@ const cartOverlay = document.querySelector('.cart-overlay')
 subheaderCart.addEventListener('click', () => {
     cartOverlay.classList.add('cart-overlay-open')
     disableScroll()
+})
+
+subheaderCart.addEventListener('keydown', (event) => {
+    if(event.key === 'Escape') {
+        cartOverlay.classList.remove('cart-overlay-open')
+        enableScroll()
+    }
 })
 
 cartOverlay.addEventListener('click', (event) => {
@@ -71,11 +82,11 @@ const getData = async () => {
     }
 }
 
-const getGoods = (callback, value) => {
+const getGoods = (callback, prop, value) => {
     getData()
         .then(data => {
             if(value) {
-                callback(data.filter(item => item.category === value)) // оставляем в data только те товары которые совпадают категорией в хешем страницы
+                callback(data.filter(item => item[prop] === value)) // оставляем в data только те товары которые совпадают категорией в хешем страницы
             } else {
                 callback(data)
             }
@@ -85,12 +96,19 @@ const getGoods = (callback, value) => {
 
 
 // если на странице нет селектора с классом ".goods__list", то попадаем в блок catch через выполнение throw (способ узнать на какой мы странице)
+// Страница категорий товаров
 try {
     
     const goodsList = document.querySelector('.goods__list')
 
     if(!goodsList) {
         throw 'This is not a goods page!'
+    }
+
+    const goodsTitle = document.querySelector('.goods__title')
+
+    const changeTitle = () => {
+        goodsTitle.textContent = document.querySelector(`[href*="#${hash}"]`).textContent // находим на странице через CSS элементы href у которых значение совпадает с нашей переменной hash
     }
 
     const createCard = data => {
@@ -142,15 +160,83 @@ try {
     window.addEventListener('hashchange', () => {
         hash = location.hash.substring(1)
 
-        if(hash === 'women') document.querySelector('.goods__title').textContent = 'Женщинам'
-        else if(hash === 'men') document.querySelector('.goods__title').textContent = 'Мужчинам'
-        else if(hash === 'kids') document.querySelector('.goods__title').textContent = 'Детям'
-
-        getGoods(renderGoodsList, hash)
+        getGoods(renderGoodsList, 'category', hash)
+        changeTitle()
     })
 
-    getGoods(renderGoodsList, hash)
-
+    changeTitle()
+    getGoods(renderGoodsList, 'category', hash)
+    
 } catch (error) {
-    console.log(error);
+    console.warn(error);
+}
+
+// Страница товара
+
+try {
+
+    if(!document.querySelector('.card-good')) {
+        throw 'This is not a card-good page!'
+    }
+
+    const cardGoodImage = document.querySelector('.card-good__image')
+    const cardGoodBrand = document.querySelector('.card-good__brand')
+    const cardGoodTitle = document.querySelector('.card-good__title')
+    const cardGoodPrice = document.querySelector('.card-good__price')
+    const cardGoodColor = document.querySelector('.card-good__color')
+    const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper')
+    const cardGoodColorList = document.querySelector('.card-good__color-list')
+    const cardGoodSizes = document.querySelector('.card-good__sizes')
+    const cardGoodSizesList = document.querySelector('.card-good__sizes-list')
+    const cardGoodBuy = document.querySelector('.card-good__buy')
+
+    const generateList = (data) => data.reduce((html, item, index) => {
+        return html + `<li class="card-good__select-item" data-id="${index}">${item}</li>`
+    }, '')
+        
+
+    const renderCardGood = ([{brand, name, cost, color, sizes, photo}]) => { // деструкраризация (data.brand и т.д)
+        cardGoodImage.src = `goods-image/${photo}`
+        cardGoodImage.alt = `${brand} ${name}`
+        cardGoodBrand.textContent = brand
+        cardGoodTitle.textContent = name
+        cardGoodPrice.textContent = `${cost} ₽`
+        if(color) {
+            cardGoodColor.textContent = color[0]
+            cardGoodColor.dataset.id = 0
+            cardGoodColorList.innerHTML = generateList(color)
+        } else {
+            cardGoodColor.style.display = 'none'
+        }
+        if(sizes) {
+            cardGoodSizes.textContent = sizes[0]
+            cardGoodSizes.dataset.id = 0
+            cardGoodSizesList.innerHTML = generateList(sizes)
+        } else {
+            cardGoodSizes.style.display = 'none'
+        }
+        
+    }
+
+    cardGoodSelectWrapper.forEach(item => {
+        item.addEventListener('click', (event) => {
+            const target = event.target
+
+            if(target.closest('.card-good__select')) {
+                target.classList.toggle('card-good__select__open')
+            }
+
+            if(target.closest('.card-good__select-item')) {
+                const cardGoodSelect = item.querySelector('.card-good__select')
+                cardGoodSelect.textContent = target.textContent
+                cardGoodSelect.dataset.id = target.dataset.id
+                cardGoodSelect.classList.remove('card-good__select__open')
+            }
+        })
+    })
+
+    getGoods(renderCardGood, 'id', hash)
+    
+} catch (error) {
+    console.warn(error);
 }
